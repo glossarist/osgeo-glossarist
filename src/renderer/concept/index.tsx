759041d1f8ccf172a19050933c6644228f28785c
Anton Strogonoff <anton@strogonoff.name>
2019-11-19 19:52:13 +0800

    feat: Commenting on terms

--- src/renderer/concept/index.tsx
+++ src/renderer/concept/index.tsx
@@ -22,6 +22,7 @@ export const Concept: React.FC<{ id: string, lang?: string }> = function ({ id }
   const [term, updateTerm] = useState('' as string);
   const [definition, updateDefinition] = useState('' as string);
   const [authSource, updateAuthSource] = useState('' as string);
+  const [comments, updateComments] = useState([] as string[]);
 
   const lang = useContext(LangConfigContext);
 
@@ -40,6 +41,7 @@ export const Concept: React.FC<{ id: string, lang?: string }> = function ({ id }
         ...concept,
         [lang.selected]: {
           ...concept[lang.selected],
+          comments: comments,
           term: term,
           definition: definition,
           authoritative_source: { link: authSource },
@@ -66,11 +68,31 @@ export const Concept: React.FC<{ id: string, lang?: string }> = function ({ id }
   useEffect(() => {
     if (concept) {
       const term = concept[lang.selected] || {};
+      updateComments(term.comments || []);
       updateTerm(term.term || '');
       updateDefinition(term.definition || '');
       updateAuthSource((term.authoritative_source || {}).link || '');
     }
   }, [concept, lang.selected]);
+
+  function handleNewComment(newComment: string) {
+    updateComments([ newComment, ...comments ]);
+    setDirty(true);
+  }
+
+  function handleCommentEdit(commentIdx: number, updatedComment: string) {
+    var newComments = [...comments];
+    newComments[commentIdx] = updatedComment;
+    updateComments(newComments);
+    setDirty(true);
+  }
+
+  function handleCommentDeletion(commentIdx: number) {
+    var newComments = [...comments];
+    newComments.splice(commentIdx, 1);
+    updateComments(newComments);
+    setDirty(true);
+  }
 
   return (
     <div className={styles.conceptBase}>
@@ -114,6 +136,18 @@ export const Concept: React.FC<{ id: string, lang?: string }> = function ({ id }
           value={definition}/>
       </div>
 
+      <div className={styles.conceptComments}>
+        <PaneHeader align="left" className={styles.conceptCommentsHeader}>
+          {comments.length} comment{(!comments.length || comments.length > 1) ? 's' : null}
+        </PaneHeader>
+
+        <Comments
+          comments={comments}
+          onCreate={handleNewComment}
+          onUpdate={handleCommentEdit}
+          onDelete={handleCommentDeletion} />
+      </div>
+
       <footer className={styles.actions}>
         <Button
           disabled={loading || !dirty}
@@ -126,3 +160,47 @@ export const Concept: React.FC<{ id: string, lang?: string }> = function ({ id }
     </div>
   );
 };
+
+
+interface CommentsProps {
+  comments: string[],
+  onCreate: (c: string) => void,
+  onUpdate: (idx: number, c: string) => void,
+  onDelete: (idx: number) => void,
+}
+const Comments: React.FC<CommentsProps> = function ({ comments, onCreate, onUpdate, onDelete }) {
+  const [newComment, updateNewComment] = useState('');
+
+  return <div className={styles.commentList}>
+    <div className={styles.comment}>
+      <EditableText
+        value={newComment}
+        placeholder="New comment…"
+        onChange={updateNewComment}
+        multiline={false}
+      />
+
+      {newComment !== ''
+        ? <Button onClick={() => {
+                onCreate(newComment);
+                updateNewComment('');
+              }}
+              className={styles.commentAddButton}
+              fill={true}
+              minimal={true}
+              intent="primary">
+            Add comment
+          </Button>
+        : null}
+    </div>
+
+    {[...comments.entries()].map(([idx, c]: [number, string]) => <Comment key={idx} content={c} />)}
+  </div>;
+}
+
+
+const Comment: React.FC<{ content: string }> = function ({ content }) {
+  return <div className={styles.comment}>
+    {content}
+  </div>
+}
